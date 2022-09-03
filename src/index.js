8451ed92ede35e58ba555f95d2e962e4ca868ae6
kevinIsomMoringa <kevin.isom@student.moringaschool.com>
2022-09-03 11:50:25 +0300

    Done Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
+const baseURl = 'http://localhost:3000/movies/'
+
+
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let inputValue = event.target.children[1].value
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        fetch(`${baseURl}${inputValue}`)
+        .then(response => {
+            if (!response.ok) {
+                // get error message from response status
+                console.error('Something went wrong',response?.status);
+                title.innerText = 'Data not found';
+                summary.innerText = 'Data not found';
+            } else if (response.ok) {
+                return response.json()
+            }
+        })
+        .then(data => {
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+        .catch(err =>  {
+            alert('There was an error!', err)
+            console.error('There was an error!', err)});
+    });
   
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
