fd69d9f313829ad4a92a730faf55b82b8e39c9f1
kevinIsomMoringa <kevin.isom@student.moringaschool.com>
2022-09-03 12:38:02 +0300

    Done Assignment

--- src/index.js
+++ src/index.js
@@ -4,30 +4,30 @@ const baseURl = 'http://localhost:3000/movies/'
 const init = () => {
     const inputForm = document.querySelector('form');
 
-    inputForm.addEventListener('submit', (event) => {
+    inputForm.addEventListener('submit', async (event) => {
         event.preventDefault();
         let inputValue = event.target.children[1].value
         const title = document.querySelector('section#movieDetails h4');
         const summary = document.querySelector('section#movieDetails p');
 
-        fetch(`${baseURl}${inputValue}`)
-        .then(response => {
+        await fetch(`${baseURl}${inputValue}`)
+        .then(async response => {
             if (!response.ok) {
                 // get error message from response status
-                console.error('Something went wrong',response?.status);
                 title.innerText = 'Data not found';
                 summary.innerText = 'Data not found';
-            } else if (response.ok) {
-                return response.json()
+                throw new Error(`Something went wrong! \nHTTP response code: ${response.status}`);    
             }
+            return await response.json();
         })
         .then(data => {
             title.innerText = data.title;
             summary.innerText = data.summary;
         })
         .catch(err =>  {
-            alert('There was an error!', err)
-            console.error('There was an error!', err)});
+            alert(err)
+            console.error(err)
+        });
     });
   
 }
